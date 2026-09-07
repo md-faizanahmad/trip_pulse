af2e7bbc55f0aae59f665e043c42aa135880aa7c
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-09-08 01:39:14 +0530

    feat(transport): add transport to destination page

--- src/app/destinations/[destination]/page.tsx
+++ src/app/destinations/[destination]/page.tsx
@@ -2,6 +2,7 @@ import Currency from "@/components/currency/Currency";
 import DestinationPlace from "@/components/destinations/DestinationPlace";
 import DestinationWeather from "@/components/destinations/DestinationWeather";
 import Places from "@/components/place/Places";
+import Transport from "@/components/transport/Transport";
 import Breadcrumb from "@/shared/Breadcrumb";
 import type { Destination } from "@/types/destination";
 import { getCurrencyByCountryCode } from "@/utils/currency";
@@ -125,6 +126,10 @@ export default async function DestinationPage({
         )}
 
         <Places
+          latitude={selectedDestination.latitude}
+          longitude={selectedDestination.longitude}
+        />
+        <Transport
           latitude={selectedDestination.latitude}
           longitude={selectedDestination.longitude}
         />
