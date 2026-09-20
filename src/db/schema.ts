3736a1813585aadb78478945b0a8f5471d5b100a
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-09-20 21:30:57 +0530

    feat: add users database schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,18 @@
-// src/db/schema.ts
+import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
-export {};
+export const users = pgTable("users", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: varchar("name", { length: 100 }).notNull(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  passwordHash: varchar("password_hash", { length: 255 }).notNull(),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+  })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at", {
+    withTimezone: true,
+  })
+    .defaultNow()
+    .notNull(),
+});
